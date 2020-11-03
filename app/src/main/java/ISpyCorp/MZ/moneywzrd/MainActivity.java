b02aa6256fe179dd66adf5acc6eaaab7bf8a6a77
FernandodoPrado <n01329973@humbermail.ca>
2020-11-02 19:48:25 -0500

    No errors build

--- app/src/main/java/ISpyCorp/MZ/moneywzrd/MainActivity.java
+++ app/src/main/java/ISpyCorp/MZ/moneywzrd/MainActivity.java
@@ -1,15 +1,12 @@
 package ISpyCorp.MZ.moneywzrd;
 
-import androidx.annotation.NonNull;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import android.annotation.SuppressLint;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -23,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.nav_home, R.id.nav_accounts, R.id.nav_settings,R.id.nav_expenses,R.id.nav_investments)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
@@ -31,4 +28,3 @@ public class MainActivity extends AppCompatActivity {
     }
 
     }
-}
